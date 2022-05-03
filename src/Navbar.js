08bde03f25e98f35e4a47bad9df071c43e165111
lohith <90506173+lohithbabloo@users.noreply.github.com>
2022-05-03 14:41:41 +0530

    Add files via upload

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="Navbar">
+            <h1>The blog site</h1>
+            <div className="links">
+                <a href="/">HOME</a>
+                <a href="/">New Blog</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
