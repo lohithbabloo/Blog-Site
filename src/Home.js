2e98172b26ecb7b03b3b1553028fed59165d8c1d
lohith <90506173+lohithbabloo@users.noreply.github.com>
2022-06-10 23:50:07 +0530

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,25 @@
+import { useState,useEffect } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+    const [blogs,setblogs] = useState(null);
+
+    const handledelete =(id) =>{
+        const newblog = blogs.filter(blog => blog.id!==id)
+        setblogs(newblog)
+    }
+    useEffect(()=>{
+        fetch('http://localhost:8000/blogs')
+        .then(res=>{
+            return res.json()
+        })
+        .then(data=>{
+            console.log(data);
+        })
+    },[])
     return (
         <div className="Home">
-            <h2>Homepage</h2>
+            {/* <BlogList blogs={blogs} title="All blogs" handledelete={handledelete} /> */}
         </div>
     );
 }
