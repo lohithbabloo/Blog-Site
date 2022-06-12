36fba91558ab187729bf1c037cbf88c3ba4672bf
Srujan Gurram <52039218+Royal-lobster@users.noreply.github.com>
2022-06-12 16:55:11 +0000

    made changes

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,18 @@ import BlogList from "./BlogList";
 import usefetch from "./usefetch";
 
 const Home = () => {
-  //   const {
-  //     data: blogs,
-  //     isloading,
-  //     errorstate,
-  //   } = usefetch("http://localhost:4000/blogs");
-  //   return (
-  //     <div className="Home">
-  //       {errorstate && <div>{errorstate}</div>}
-  //       {blogs && <BlogList blogs={blogs} title="All blogs" />}
-  //       {isloading && <div>isloading....</div>}
-  //     </div>
-  //   );
-  return <h1>hi</h1>;
+  const {
+    data: blogs,
+    isloading,
+    errorstate,
+  } = usefetch("http://localhost:4000/blogs");
+  return (
+    <div className="Home">
+      {errorstate && <div>{errorstate}</div>}
+      {blogs && <BlogList blogs={blogs} title="All blogs" />}
+      {isloading && <div>isloading....</div>}
+    </div>
+  );
 };
 
 export default Home;
